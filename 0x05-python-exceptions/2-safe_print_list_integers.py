6ff579b30942e0aa90ae74628d8b2c4023222e46
emmyq10 <emmatunde2001@gmail.com>
2023-08-30 12:12:44 -0700

    all tsk completed

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list:
+            if isinstance(element, int):
+                print("{:d}".format(element), end=' ')
+                count += 1
+                if count >= x:
+                    break
+        print()
+    except:
+        pass
+    return count
