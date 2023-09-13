8af375b0930ff9a9b43e47b13dfff7b4de8f24f4
emmyq10 <emmatunde2001@gmail.com>
2023-09-13 11:00:49 -0700

    redo task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,13 @@
-class MyList(list):
+#!/usr/bin/python3
 
-    """my_list class"""
+"""my_list class"""
+
+
+class MyList(list):
+    """A class that inherits from a list"""
 
 
     def print_sorted(self):
-        """A class that inherits from a list"""
-
-        sorted_list = sorted(self)
         """This prints the list, but sorted (ascending sort)"""
+        sorted_list = sorted(self)
         print(sorted_list)
